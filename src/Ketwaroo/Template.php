210759720062f98887357936b9fdd6c906ceb3e3
Yaasir Ketwaroo <608793+ketwaroo@users.noreply.github.com>
2018-07-18 23:17:03 -0400

    Added      composer.json Added      src/Ketwaroo/Template.php Initial commit

---
+++ src/Ketwaroo/Template.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Ketwaroo;
+
+
+
+/**
+ * Description of Template
+ *
+ * @author Yaasir Ketwaroo <ketwaroo.yaasir at gmail>
+ */
+class Template
+{
+    use Pattern\TraitSingleton;
+    
+    
+}
